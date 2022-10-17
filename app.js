37d3cf283ab8d5115e262d5e1dbc68c3a84ab7f5
shubham0419 <shubham.e21@nsut.ac.in>
2022-10-17 16:19:08 +0530

    login/logout vulneraity fixid

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+// const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -65,19 +64,19 @@ passport.deserializeUser((id,done) => {
   });
 });
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/personal-blog",
-  userProfileURL:"https://www.googlepis.com/oauth2/v3/userinfo",
-  passReqToCallback: true
-},
-function(request, accessToken, refreshToken, profile, done) {
-  User.findOrCreate({ googleId: profile.id }, (err, user) => {
-    return done(err, user);
-  });
-}
-));
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.CLIENT_ID,
+//   clientSecret: process.env.CLIENT_SECRET,
+//   callbackURL: "http://localhost:3000/auth/google/personal-blog",
+//   userProfileURL:"https://www.googlepis.com/oauth2/v3/userinfo",
+//   passReqToCallback: true
+// },
+// function(request, accessToken, refreshToken, profile, done) {
+//   User.findOrCreate({ googleId: profile.id }, (err, user) => {
+//     return done(err, user);
+//   });
+// }
+// ));
 
 app.get("/", (req,res)=>{
   User.find({blog:{$ne:null}},function(err,foundUser){
@@ -191,23 +190,20 @@ app.post("/login",(req,res) => {
 
     req.login(newUser,(err) => {
       if(err){
-        console.log(err);
         res.redirect("/login");
     }else{
-        passport.authenticate("local")(req,res,function(err){
-          if(err){
-            res.redirect("/login");
-          }else{
-            res.redirect("/");
-          }
+        passport.authenticate("local")(req,res,function(){
+          res.redirect("/");
         });
     }
     });
 });
 
-app.get("/logout",(req,res) => {
-  req.logOut();
-  res.redirect("/");
+app.get("/logout",function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
 });
 
 
